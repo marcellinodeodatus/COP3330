43e57ca007a2a1c061dd1bc61bd3c5e6fe72b5cb
dmarcellino <deodatusmarcellino@Knights.ucf.edu>
2020-09-12 15:29:18 -0400

    Complete Encrypt Class

--- marcellino_p1/src/Encrypter.java
+++ marcellino_p1/src/Encrypter.java
@@ -1,34 +1,55 @@
+
 public class Encrypter {
 
-    static String encrypt(String number){
+    public static String encrypt(String number){
 
-        int []num = new int[4];
-
-        for(int i = 0; i < 4; i++){
-            char ch = number.charAt(i);
-            num[i] = Character.getNumericValue(ch);
-        }
-
-        for (int i = 0; i < 4; i++){
-            int temp = num[i];
-            temp += 7;
-            temp = temp % 10;
-            num[i] = temp;
-        }
-        int temp = num[0];
-        num[0] = num[2];
-        num[2] = temp;
-        temp = num[1];
-        num[1] = num[3];
-        num[3] = temp;
-        int newNumber = 0;
-        for(int i = 0; i <4; i++){
-            newNumber = newNumber * 10 + num[i];
-        }
-        String output = Integer.toString(newNumber);
-        if(num[0] == 0)
-            output = "0" + output;
+        int []num = convertString2IntArray(number);
+        int newNumber = calculation(num);
+        String output = convertInt2String(newNumber, num);
+        printOutput(number, output);
         return output;
     }
 
-} // end class Encrypter
+    public static void printOutput(String num, String output) {
+        System.out.println("encrypt " +num+ " to "+output);
+    }
+
+    public static int [] convertString2IntArray(String number){
+        int[]arr = new int[4];
+        for(int i = 0; i < 4; i++){
+            char ch = number.charAt(i);
+            arr[i] = Character.getNumericValue(ch);
+        }
+        return arr;
+    }
+
+    public static int calculation(int [] arrayOfNumber){
+        for (int i = 0; i < 4; i++){
+            int temp = arrayOfNumber[i];
+            temp += 7;
+            temp = temp % 10;
+            arrayOfNumber[i] = temp;
+        }
+        int temp = arrayOfNumber[0];
+        arrayOfNumber[0] = arrayOfNumber[2];
+        arrayOfNumber[2] = temp;
+        temp = arrayOfNumber[1];
+        arrayOfNumber[1] = arrayOfNumber[3];
+        arrayOfNumber[3] = temp;
+
+        int newNumber = 0;
+        for(int i = 0; i <4; i++){
+            newNumber = newNumber * 10 + arrayOfNumber[i];
+        }
+        return newNumber;
+
+    }
+
+    public static String convertInt2String(int number, int []arrayOfNumber){
+        String numberString = Integer.toString(number);
+        if(arrayOfNumber[0] == 0)
+            numberString = "0" + numberString;
+        return numberString;
+    }
+
+}
--- marcellino_p1/src/Tester.java
+++ marcellino_p1/src/Tester.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a 4 digit integer:");
-        String number = sc.nextLine();
-        String encryptedNumber = Encrypter.encrypt(number);
-        System.out.println("encrypts " +number+" to "+encryptedNumber);
+        Encrypter myEncrypter = new Encrypter();
+        String encryptedNumber = myEncrypter.encrypt("1234");
     }
 }
