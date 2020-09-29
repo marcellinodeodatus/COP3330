bf1e6962987723d00b622f5fcc112d15f5d236e1
dmarcellino <deodatusmarcellino@Knights.ucf.edu>
2020-09-28 23:58:32 -0400

    add BodyMassIndexTest.java

--- marcellino_p2/src/BodyMassIndexTest.java
+++ marcellino_p2/src/BodyMassIndexTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BodyMassIndexTest {
+
+    public void testBodyMassIndex(){
+        BodyMassIndex b = new BodyMassIndex(10, 20);
+
+        Assertions.assertEquals(10, b.getHeight());
+        Assertions.assertEquals(20, b.getWeight());
+    }
+
+    public void testCalculateBMI(){
+        BodyMassIndex b = new BodyMassIndex(63, 150);
+
+        double testBMI = b.calculateBMI();
+
+        Assertions.assertEquals(26, testBMI);
+    }
 }
